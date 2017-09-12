a883769dfdd553eb9fec2624c3cb327f9fabee1a
Kyle Maxwell <KMaxwell@seic.com>
2017-09-12 10:26:01 -0400

    master detail angular docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroe!';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  {id: 11,name: 'Firestorm'},
+  {id: 12,name: 'Icestorm'},
+  {id: 13,name: 'Sandstorm'},
+  {id: 14,name: 'Earthstorm'},
+  {id: 15,name: 'Stormstorm'},
+  {id: 16,name: 'Brimstorm'},
+  {id: 17,name: 'Waterstorm'},
+  {id: 18,name: 'Airstorm'},
+  {id: 19,name: 'Pebblestorm'},
+  {id: 20,name: 'Fiststorm'},
+]
