281aade1a56f035aebe8421d114cf6adeebfcd64
Kyle Maxwell <KMaxwell@seic.com>
2017-09-18 10:25:40 -0400

    added activated route, param map, location, rxjs, and hero service. along with go back method and added to template.

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from '../services/hero.service';
 import { Hero } from '../models/hero.model';
 
 @Component({
@@ -10,9 +15,18 @@ export class HeroDetailsComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor( private heroService: HeroService, private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    this.route.paramMap
+      .switchMap((params: ParamMap) =>
+      this.heroService.getHero(+params.get('id')))
+        .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
