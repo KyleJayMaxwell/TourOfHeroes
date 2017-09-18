b22c153392c3c67b2b6b0c43294d89bbadeb4962
Kyle Maxwell <KMaxwell@seic.com>
2017-09-18 10:54:40 -0400

    made selected hero preview before going to detail, added click method gotoDetail for heroes routing to details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hero } from '../models/hero.model';
 import { Heroes } from '../proxies/mock-heroes.proxy';
 import { HeroService } from '../services/hero.service';
@@ -10,9 +11,9 @@ import { HeroService } from '../services/hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private router: Router) { }
 
-  title = 'Tour of Heroe!';
+  title = 'Tour of Heroes!';
   heroes: Hero[];
   selectedHero: Hero;
 
@@ -22,6 +23,10 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail():void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
   ngOnInit(): void {
